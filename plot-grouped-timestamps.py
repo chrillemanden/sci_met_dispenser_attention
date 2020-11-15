b8c38f7b1a1b1a503e0be8c30d889479e7e90998
chrillemanden <christian@vetdevice.com>
2020-11-15 10:41:51 +0100

    Update p-g-t.py and include new data

--- plot-grouped-timestamps.py
+++ plot-grouped-timestamps.py
@@ -1,10 +1,19 @@
-#import matplotlib
+import matplotlib.pyplot as plt
 import csv
 
-with open ('activations.csv') as csv_file:
+year = 2020
+activations = {}
+
+with open ('activations_latest.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     lines = 0
     for row in csv_reader:
         lines += 1
+        activations[row[1]] = activations.get(row[1], 0) + 1
+        #print (row)
 
 print(lines)
+print (sum(activations.values()))
+print (activations)
+
+plt.bar(myDictionary.keys(), myDictionary.values(), width, color='g')
