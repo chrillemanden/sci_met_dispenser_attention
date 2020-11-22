9b2775d2fc35b4c6bb6377a38f16317166804f03
chrillemanden <christian@vetdevice.com>
2020-11-22 21:45:29 +0100

    Update for simple data visualisation

--- plot-grouped-timestamps.py
+++ plot-grouped-timestamps.py
@@ -4,7 +4,7 @@ import csv
 year = 2020
 activations = {}
 
-with open ('activations_latest.csv') as csv_file:
+with open ('activations_all.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     lines = 0
     for row in csv_reader:
@@ -12,8 +12,32 @@ with open ('activations_latest.csv') as csv_file:
         activations[row[1]] = activations.get(row[1], 0) + 1
         #print (row)
 
+test_file = open("test_file.csv", "w+")
+for key, value in activations.items():
+    test_file.write("%s,%s\n" % (key, value))
+
+
 print(lines)
 print (sum(activations.values()))
 print (activations)
 
-plt.bar(myDictionary.keys(), myDictionary.values(), width, color='g')
+
+
+dates = activations.keys()
+values = activations.values()
+# print(dates)
+# print(values)
+
+# for i, d in enumerate(dates):
+#     dates[i] = str(i) + d
+
+# print(dates)
+# plt.bar(dates, values, 0.5, color='g')
+# plt.show()
+#ax = plt.subplot(111)
+#ax.bar(dates, values, 0.5, color='g')
+
+#ax.xaxis_date()
+
+# plt.bar(*zip(*activations.items()))
+# plt.show()
